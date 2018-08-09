0868d0c19e060ddacabcbbc61eb554c830f2515d
Anastasia Lalamentik <anastasialalamentik@gmail.com>
2018-08-09 11:03:48 -0400

    Final

---
+++ routeData.js
@@ -0,0 +1,51 @@
+var routeData = {
+    "Eastbound": {
+      "60": {
+        "destination" : "Randolph/Habror Dr",
+        "frequencyInMins" : "20"
+      },
+      "124": {
+        "destination" : "Navy Pier",
+        "frequencyInMins" : "10"
+      }
+    },
+    "Northbound": {
+      "151": {
+        "destination" : "Devon/Clark",
+        "frequencyInMins" : "16"
+        }
+    },
+    "Southbound": {
+      "J14": {
+        "destination" : "103rd/Stony Island",
+        "frequencyInMins" : "15"
+      },"124": {
+        "destination" : "Navy Pier",
+        "frequencyInMins" : "10"
+        }
+    },
+    "Reroute":{
+      "6x":{
+        "destination" : "Jackson Park Express",
+        "frequencyInMins" : "N/A"
+      }
+    },
+    "Reroute":{
+      "6x":{
+        "destination" : "Jackson Park Express",
+        "frequencyInMins" : "N/A"
+      }
+    },
+    "Out of Service":{
+      "4":{
+        "destination" : "Cottage Grove",
+        "frequencyInMins" : "Resumes 12:10am"
+      },"20":{
+        "destination" : "Madison",
+        "frequencyInMins" : "Resumes 12:10am"
+      },"157":{
+        "destination" : "Streeterville/Taylor",
+        "frequencyInMins" : "Resumes 6:00am"
+      }
+    }
+  }
