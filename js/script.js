a92a2fee59238bebb418748380ce658b8d69ac8e
Anastasia Lalamentik <anastasialalamentik@gmail.com>
2018-08-13 15:13:46 -0400

    Fixes

---
+++ js/script.js
@@ -0,0 +1,33 @@
+  $(document).ready(function(){
+    var scheduleDetailIconHtml = "<div class='scheduleDetailIcon' id='rightArrow'><img src='images/icon-right-arrow.png'></div>";
+    for (var i in routeData){
+      var $directionHeader = $("<div class='directionHeader'></div>");
+      $directionHeader.text(i);
+      $(".schedulesList").append($directionHeader);
+      for (var j in routeData[i]){
+        var $scheduleItem = $("<div class='scheduleItem'></div>");
+        var busLineHtml = "<div class='busLine'>" + j + "</div>";
+        var lineDirectionHtml = "<div class='scheduleContent'><div class='lineDirection'>To " + routeData[i][j]["destination"] + "</div>";
+        if (i == "Reroute" || i == "Out of Service"){
+          lineDirectionHtml += "<div class='arrivalTimes'>" + routeData[i][j]["frequencyInMins"] + "</div>";
+        } else {
+          lineDirectionHtml += buildArrivalTimesHtml(routeData[i][j]["frequencyInMins"]);
+        }
+        lineDirectionHtml += "</div>";
+        $scheduleItem.html(busLineHtml + lineDirectionHtml + scheduleDetailIconHtml);
+        $(".schedulesList").append($scheduleItem);
+      }
+    }
+
+    function buildArrivalTimesHtml(frequency){
+      var randomCount = Math.floor(Math.random() * 4) + 1;
+      var randomMin = 0 - (Math.floor(Math.random() * 4) + 1);
+      var arrivalHtml = "<div class='arrivalTimes'>";
+      for (var i = 0; i < randomCount; i++){
+        var displayMins = randomMin + (frequency * (i + 1));
+        arrivalHtml += "<div class='arrival'>" + displayMins + " mins</div>";
+      }
+      arrivalHtml += "</div>";
+      return arrivalHtml;
+    }
+  });
